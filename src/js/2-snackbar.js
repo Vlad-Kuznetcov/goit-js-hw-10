95ebb595783d483074421ab13d29d73bb9bf8297
Vlad-Kuznetcov <vladonik.909@gmail.com>
2024-07-20 13:05:05 +0300

    123

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,40 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const refs = {
+  formEl: document.querySelector('.form'),
+  btnEl: document.querySelector('button'),
+};
+
+refs.formEl.addEventListener('submit', e => {
+  e.preventDefault();
+  const formData = new FormData(refs.formEl);
+  const delay = formData.get('delay');
+  const status = formData.get('state');
+
+  const isSuccess = status === 'fulfilled';
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (isSuccess) {
+        resolve();
+      } else {
+        reject();
+      }
+    }, delay);
+  });
+
+  promise
+    .then(value =>
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms
+`,
+      })
+    )
+    .catch(error =>
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms
+`,
+      })
+    );
+});
