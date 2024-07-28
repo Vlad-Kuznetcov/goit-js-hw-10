9e8cae7ffa9022b2355f3714983db84a0dd5595b
Vlad-Kuznetcov <vladonik.909@gmail.com>
2024-07-28 14:23:13 +0000

    Deploying to gh-pages from @ Vlad-Kuznetcov/goit-js-hw-10@b9a1270c8ff5f3ada2e05fbc133df5ca1c62025a 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{i as o}from"./assets/vendor-77e16229.js";const r={formEl:document.querySelector(".form"),btnEl:document.querySelector("button")};r.formEl.addEventListener("submit",m=>{m.preventDefault();const s=new FormData(r.formEl),e=s.get("delay"),i=s.get("state")==="fulfilled";new Promise((t,n)=>{setTimeout(()=>{i?t():n()},e)}).then(t=>o.success({message:`✅ Fulfilled promise in ${e}ms
-`})).catch(t=>o.error({message:`❌ Rejected promise in ${e}ms
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{i as o}from"./assets/vendor-77e16229.js";const r={form:document.querySelector(".form")};r.form.addEventListener("submit",m=>{m.preventDefault();const t=new FormData(r.form),e=t.get("delay"),i=t.get("state")==="fulfilled";new Promise((s,a)=>{setTimeout(()=>{i?s():a()},e)}).then(s=>o.success({message:`✅ Fulfilled promise in ${e}ms
+`})).catch(s=>o.error({message:`❌ Rejected promise in ${e}ms
 `}))});
 //# sourceMappingURL=commonHelpers2.js.map
